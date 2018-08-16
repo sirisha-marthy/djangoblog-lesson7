41881472d86b51497ab3bee9af52e17639db1695
Sirisha Marthy <sirisham@amazon.com>
2018-08-16 16:31:09 -0700

    Lesson7 submission

--- djangoblog/mysite/myblog/admin.py
+++ djangoblog/mysite/myblog/admin.py
@@ -2,6 +2,16 @@ from django.contrib import admin
 from myblog.models import Post
 from myblog.models import Category
 
+class CategoryInline(admin.TabularInline):
+    model = Category.posts.through   # https://docs.djangoproject.com/en/dev/ref/contrib/admin/#working-with-many-to-many-models
 
-admin.site.register(Post)
-admin.site.register(Category)
+class PostAdmin(admin.ModelAdmin):
+    inlines = [CategoryInline]
+
+class CategoryAdmin(admin.ModelAdmin):
+    exclude = ('posts',)
+    pass
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Category, CategoryAdmin)
+
